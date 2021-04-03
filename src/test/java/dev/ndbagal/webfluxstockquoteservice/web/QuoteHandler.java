4a91b9842545157cad803dd5a106a384dd0f55a1
Navnath Bagal <me@ndbagal.dev>
2021-04-03 15:44:11 +0530

    Created spring webflux quote handler

---
+++ src/test/java/dev/ndbagal/webfluxstockquoteservice/web/QuoteHandler.java
@@ -0,0 +1,29 @@
+package dev.ndbagal.webfluxstockquoteservice.web;
+
+import java.time.Duration;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import dev.ndbagal.webfluxstockquoteservice.model.Quote;
+import dev.ndbagal.webfluxstockquoteservice.service.QuoteGeneratorService;
+import reactor.core.publisher.Mono;
+
+@Component
+public class QuoteHandler {
+
+  private final QuoteGeneratorService quoteGeneratorService;
+
+  public QuoteHandler(QuoteGeneratorService quoteGeneratorService) {
+    this.quoteGeneratorService = quoteGeneratorService;
+  }
+
+  public Mono<ServerResponse> fetchQuotes(ServerRequest request) {
+    int size = Integer.parseInt(request.queryParam("size").orElse("10"));
+
+    return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+        .body(this.quoteGeneratorService.fetchQuoteStream(Duration.ofMillis(100)).take(size), Quote.class);
+  }
+}
