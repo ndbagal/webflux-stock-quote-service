98e3b8f6dacda9bd928c1c72c725fcecc24e1b82
Navnath Bagal <me@ndbagal.dev>
2021-04-03 15:51:47 +0530

    Created quote router

---
+++ src/test/java/dev/ndbagal/webfluxstockquoteservice/web/QuoteRouter.java
@@ -0,0 +1,20 @@
+package dev.ndbagal.webfluxstockquoteservice.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class QuoteRouter {
+
+  @Bean
+  public RouterFunction<ServerResponse> route(QuoteHandler handler) {
+    return RouterFunctions.route(
+        RequestPredicates.GET("/quotes").and(RequestPredicates.accept(MediaType.APPLICATION_JSON)),
+        handler::fetchQuotes);
+  }
+}
