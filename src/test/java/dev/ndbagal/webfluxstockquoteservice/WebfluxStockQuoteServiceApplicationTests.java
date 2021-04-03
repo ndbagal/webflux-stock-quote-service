a280c4cdf6c79ac4dc05db1d84122d10213c5887
Navnath Bagal <me@ndbagal.dev>
2021-04-03 13:11:12 +0530

    Initial commit

---
+++ src/test/java/dev/ndbagal/webfluxstockquoteservice/WebfluxStockQuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ndbagal.webfluxstockquoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxStockQuoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
